989165b8e969d5366ee4ad89a4ad349b15947fb7
myusa2 <myusa.95.222@gmail.com>
2020-06-06 12:44:13 +0900

    modintの出力関数を追加

--- modint.cc
+++ modint.cc
@@ -1,33 +1,9 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <algorithm>
-#include <numeric>
-#include <string>
-#include <sstream>
-#include <cmath>
-#include <cstring>
-#include <assert.h>
-#include <utility>
-#include <tuple>
-#include <array>
-#include <bitset>
-#include <cstdlib>
-#include <complex>
+#include <bits/stdc++.h>
 
 using int64 = long long;
 using uint64 = unsigned long long;
 
 using namespace std;
-
-constexpr int64 MOD = 1e9 + 7;
 
 template <int64 M>
 struct ModInt {
@@ -71,6 +47,11 @@ struct ModInt {
         return pow(*this, M - 2);
     }
 
+    friend ostream& operator<<(ostream& os, const ModInt<M>& m) {
+        os << m.n;
+        return os;
+    }
+
     static ModInt pow(ModInt a, int64 n) {
         ModInt res = 1;
         while (n) {
@@ -84,53 +65,34 @@ struct ModInt {
     }
 };
 
-namespace CombUtil {
+constexpr int MAX_N = 3000;
+constexpr int MAX_S = 3000;
+constexpr int64 MOD = 998244353;
 
-constexpr int64 M = MOD;
-
-vector<ModInt<M>> fact;
-vector<ModInt<M>> inv_fact;
-
-static void setup(int max_n) {
-    fact.resize(max_n + 1);
-    inv_fact.resize(max_n + 1);
-
-    fact[0] = 1;
-    for (int n = 1; n <= max_n; n++) {
-        fact[n] = fact[n - 1] * n;
-    }
-
-    inv_fact[max_n] = fact[max_n].inv();
-    for (int n = max_n - 1; n >= 0; n--) {
-        inv_fact[n] = inv_fact[n + 1] * (n + 1);
-    }
-}
-
-ModInt<M> choose(int m, int n) {
-    if (m < n) return 0;
-    return fact[m] * inv_fact[m - n] * inv_fact[n];
-}
-
-};  // namespace CombUtil
-
-namespace CU = CombUtil;
-
-ModInt<CU::M> cumsum(int r, int c) {
-    return CU::choose(r + c + 2, r + 1) - CU::choose(r, 0);
-}
+ModInt<MOD> dp[MAX_N + 1][MAX_S + 1];
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    cout << fixed << setprecision(10);
 
-    int r1, c1, r2, c2;
-    cin >> r1 >> c1 >> r2 >> c2;
+    int n, s;
+    cin >> n >> s;
 
-    CU::setup(r2 + c2 + 2);
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 
-    auto ans = cumsum(r2, c2) - cumsum(r1 - 1, c2) - cumsum(r2, c1 - 1) + cumsum(r1 - 1, c1 - 1);
-    cout << ans.n << endl;
+    dp[0][0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= s; j++) {
+            dp[i + 1][j] += dp[i][j] * 2;
+            if (j + a[i] <= s) {
+                dp[i + 1][j + a[i]] += dp[i][j];
+            }
+        }
+    }
+    cout << dp[n][s] << endl;
 
     return 0;
 }
